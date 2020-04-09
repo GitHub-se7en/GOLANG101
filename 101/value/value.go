7661090c513501e880e492499c2e5f97ad4b223b
Github-se7en <movieqizongzui@163.com>
2020-04-09 10:11:46 +0800

    由于编译报错 ， 注释掉之前故意写错的代码

--- 101/value/value.go
+++ 101/value/value.go
@@ -11,8 +11,8 @@ func Aaaa() {
 	d1 := 34
 	fmt.Print(s1)
 	fmt.Print(d1)
-	s4 string = "sdf"
+	//下面这行编译会报错的，不能把struct里面的格式声明变量
+	//s4 string = "sdf"
 	//s2 = 34
-	fmt.Print(s2)
+	//fmt.Print(s2)
 }
-
