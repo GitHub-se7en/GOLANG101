98f160e578968f2810873b2c0b3a12ff8f8d7330
Github-se7en <movieqizongzui@163.com>
2020-04-10 10:03:21 +0800

    添加所有项目的项目介绍，添加多个类型实现同一个接口类型的案例

---
+++ 101/multiinterface/multiInterface2.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//我升级一下相关的接口
+
+type a interface {
+	print()
+}
+type b struct {
+	a
+}
+
+func (b *b) print() {
+	b.a.print()
+}
+func test(a a) {
+	i := b{a}
+	i.print()
+}
+func test2(a a) {
+	a.print()
+}
+
+type c struct {
+}
+
+//test一定是需要是接口类型的，为什么外面还需要包一层struct，岂不是多此一举
+//
+func (c *c) print() {
+	fmt.Println("我是c实现的print方法")
+}
+func main() {
+	//test(&c{})
+	//记住只有c的指针类型实现了接口，c类型并没有实现
+	test2(&c{})
+}
